082cadb6427075e8b6fef20dec98ed7e10b28add
Magno <magno.luque@upch.pe>
2024-04-27 19:25:17 -0500

    change

--- pages/2_Subir_Plan_de_Estudios.py
+++ pages/2_Subir_Plan_de_Estudios.py
@@ -2,8 +2,29 @@ import streamlit as st
 import pandas as pd
 
 from login import autenticacion_usuario
-from readDataFrame import leerDataFrame
 
+# Función que permite verificar que los nombres de las columnas, como parámetro, estén en el DataFrame
+def verificarDataFrame(df):
+    nombresColumnas = ['Ciclo', 'Código', 'Nombre', 'Requisito', 'Nombre Requisito', 'Tipo', 'Sede', 'Modalidad', 'Créditos']
+    for columna in nombresColumnas:
+        if columna not in df.columns:
+            raise ValueError(f"El DataFrame no tiene la columna requerida: {columna}")
+    return df
+
+# Función que permite la lectura de DataFrame
+def leerDataFrame(df):
+
+    extension = df.split('.')[-1]
+    if extension == 'csv':
+        data = pd.read_csv(df)
+    elif extension == 'xlsx':
+        data = pd.read_excel(df)
+    else:
+        raise ValueError("¡Este tipo de archivo no es admitido!")
+
+    verificarDataFrame(data)
+
+    return data
 
 def main():
     if autenticacion_usuario():
@@ -11,6 +32,7 @@ def main():
         upload_file = st.file_uploader('Subir el plan de estudios correspondiente a tu carrera')
         if upload_file is not None:
             df = leerDataFrame(upload_file)
+         
             st.session_state['df']= df
     else:
         st.error("Debes iniciar sesión para ver el contenido.")
