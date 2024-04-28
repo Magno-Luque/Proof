0d427393bb1e79e0a42911d24e90772b13cab36f
Magno <magno.luque@upch.pe>
2024-04-27 21:07:35 -0500

    change

--- pages/3_Plan_de_Estudios.py
+++ pages/3_Plan_de_Estudios.py
@@ -105,8 +105,6 @@ def mostrarGrafo(acronimos,posic):
 def main():
     if autenticacion_usuario():
         st.title("Plan de estudios")
-        st.subheader("Grafo del plan de estudios de la carrera Ing. Informática(Grafo Magno)")
-        # Chequea primero si hay un df activo, si no, te sale un mensaje y no un error
         if 'df' not in st.session_state:
             st.error("Primero carga el Plan de Estudios")
         else:
